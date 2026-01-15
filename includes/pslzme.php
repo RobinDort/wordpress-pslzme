53a1de00970f63865b8107c8a18df38023a2050b
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-01-15 12:57:10 +0100

    fixed getter function call

--- includes/pslzme.php
+++ includes/pslzme.php
@@ -152,7 +152,7 @@ class Pslzme {
 	 */
 	private function define_admin_hooks() {
 
-		$plugin_admin = new Pslzme_Admin( $this->get_plugin_name(), $this->get_version() );
+		$plugin_admin = new Pslzme_Admin( $this->get_pslzme(), $this->get_version() );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
