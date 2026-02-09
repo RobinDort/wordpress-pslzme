dde4bb06e21ae33ffeef06b3b40d8ff77f70d4f7
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-02-09 15:26:06 +0100

    Added a content attribute to switch between image and video content

--- build/pslzme-content/index.asset.php
+++ build/pslzme-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '25a7c58ad41c56bfa742');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n'), 'version' => '0763eb7081cf04db12fc');
--- src/pslzme-content/edit.js
+++ src/pslzme-content/edit.js
@@ -31,66 +31,90 @@ export default function Edit({ attributes, setAttributes }) {
 		<div {...blockProps}>
 			<InspectorControls>
 				<Panel>
-					<PanelBody title={__("Personalized Content Section", "pslzme")}>
-						<MediaUploadCheck>
-							<MediaUpload
-								onSelect={(image) => {
-									setAttributes({ personalized_image: { id: image.id, url: image.url } });
-								}}
-								allowedTypes={["image/jpeg", "image/png", "image/svg"]}
-								value={attributes.personalized_image.id}
-								render={({ open }) => (
-									<>
-										{!attributes.personalized_image.id && (
-											<Button isSecondary onClick={open}>
-												{__("Pslzme content personalized image", "pslzme")}
-											</Button>
-										)}
-										{!!attributes.personalized_image.id && attributes.personalized_image.id && (
-											<>
-												<img src={attributes.personalized_image.url} onClick={open} />
-												<Button isLink isDestructive onClick={() => setAttributes({ personalized_image: { id: 0, url: "" } })}>
-													{__("Delete image", "pslzme")}
-												</Button>
-											</>
-										)}
-									</>
-								)}
-							/>
-						</MediaUploadCheck>
-
-						<TextControl
-							label={__("Pslzme content personalized image alt text", "pslzme")}
-							value={attributes.personalized_image_alt}
-							onChange={(value) => setAttributes({ personalized_image_alt: value })}
-						/>
-
+					<PanelBody>
 						<SelectControl
-							label={__("Pslzme content personalized image size", "pslzme")}
-							value={attributes.personalized_image_size}
-							options={[...imageSizeOptions]}
-							onChange={(value) => setAttributes({ personalized_image_size: value })}
-						/>
-
-						<TextareaControl
-							label={__("Pslzme content personalized image caption", "pslzme")}
-							rows={2}
-							value={attributes.personalized_image_caption}
-							onChange={(value) => setAttributes({ personalized_image_caption: value })}
-						/>
-
-						<SelectControl
-							label={__("Pslzme content personalized image link", "pslzme")}
-							value={attributes.personalized_image_link}
-							options={pageOptions}
-							onChange={(value) =>
-								setAttributes({
-									personalized_image_link: parseInt(value),
-								})
-							}
+							label={__("Pslzme content type", "pslzme")}
+							value={attributes.content_type}
+							options={[
+								{ label: __("Image", "pslzme"), value: "image" },
+								{ label: __("Video", "pslzme"), value: "video" },
+							]}
+							onChange={(value) => setAttributes({ content_type: value })}
 						/>
 					</PanelBody>
 				</Panel>
+
+				{attributes.content_type === "image" && (
+					<Panel>
+						<PanelBody title={__("Pslzme content personalized image settings", "pslzme")}>
+							<MediaUploadCheck>
+								<MediaUpload
+									onSelect={(image) => {
+										setAttributes({ personalized_image: { id: image.id, url: image.url } });
+									}}
+									allowedTypes={["image/jpeg", "image/png", "image/svg"]}
+									value={attributes.personalized_image.id}
+									render={({ open }) => (
+										<>
+											{!attributes.personalized_image.id && (
+												<Button isSecondary onClick={open}>
+													{__("Pslzme content personalized image", "pslzme")}
+												</Button>
+											)}
+											{!!attributes.personalized_image.id && attributes.personalized_image.id && (
+												<>
+													<img src={attributes.personalized_image.url} onClick={open} />
+													<Button isLink isDestructive onClick={() => setAttributes({ personalized_image: { id: 0, url: "" } })}>
+														{__("Delete image", "pslzme")}
+													</Button>
+												</>
+											)}
+										</>
+									)}
+								/>
+							</MediaUploadCheck>
+
+							<TextControl
+								label={__("Pslzme content personalized image alt text", "pslzme")}
+								value={attributes.personalized_image_alt}
+								onChange={(value) => setAttributes({ personalized_image_alt: value })}
+							/>
+
+							<SelectControl
+								label={__("Pslzme content personalized image size", "pslzme")}
+								value={attributes.personalized_image_size}
+								options={[...imageSizeOptions]}
+								onChange={(value) => setAttributes({ personalized_image_size: value })}
+							/>
+
+							<TextareaControl
+								label={__("Pslzme content personalized image caption", "pslzme")}
+								rows={2}
+								value={attributes.personalized_image_caption}
+								onChange={(value) => setAttributes({ personalized_image_caption: value })}
+							/>
+
+							<SelectControl
+								label={__("Pslzme content personalized image link", "pslzme")}
+								value={attributes.personalized_image_link}
+								options={pageOptions}
+								onChange={(value) =>
+									setAttributes({
+										personalized_image_link: parseInt(value),
+									})
+								}
+							/>
+						</PanelBody>
+					</Panel>
+				)}
+
+				{attributes.content_type === "video" && (
+					<Panel>
+						<PanelBody title={__("Pslzme content personalized video settings", "pslzme")}>
+							<TextControl label={__("Pslzme content personalized video URL", "pslzme")} value="" />
+						</PanelBody>
+					</Panel>
+				)}
 			</InspectorControls>
 		</div>
 	);
