a432aa03d227aaaeb59e69edb997e9d94cee53a9
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-02-11 15:00:41 +0100

    Added documentation for the ElementorWidgetPslzmeText class

--- public/elementor/pslzme-public-elementor-pslzme-text.php
+++ public/elementor/pslzme-public-elementor-pslzme-text.php
@@ -1,36 +1,64 @@
 <?php
 
+/**
+ * Class that creates a custom elementor widget as pslzme text.
+ */
 class ElementorWidgetPslzmeText extends \Elementor\Widget_Base {
 
+	/**
+     * This function returns the name of the widget.
+     */
     public function get_name(): string {
         return 'pslzme_text';
     }
 
+	/**
+     * This function returns the title of the widget.
+     */
     public function get_title(): string {
         return esc_html__("Pslzme Text Widget", "pslzme");
     }
 
+	/**
+     * This function returns the icon of the widget.
+     */
     public function get_icon(): string {
         return 'eicon-t-letter-bold';
     }
 
+	/**
+     * This function returns the categories of the widget.
+     */
     public function get_categories(): array {
 		return [ 'Pslzme' ];
 	}
 
+	/**
+     * This function returns the keywords of the widget.
+     */
     public function get_keywords(): array {
 		return [ 'Pslzme', 'pslzme', 'Text', 'text', 'Pslzme Text', 'pslzme text' ];
 	}
 
+	/**
+     * This function registers custom controls for the widget.
+     */
     protected function register_controls(): void {
         $this->add_content_controls();
     }
 
+	/**
+     * This function renders the widget and outputs its content.
+     * @location /public/partials/pslzme-public-elementor-pslzme-text.php'
+     */
     protected function render(): void {
         $settings = $this->get_settings_for_display();
         require_once plugin_dir_path( dirname( __FILE__ ) ) . 'partials/pslzme-public-elementor-pslzme-text.php';
     }
 
+	/**
+     * This functions adds sections and control options to the widget.
+     */
     private function add_content_controls(): void {
         $this->start_controls_section(
 			'content_section_personalized_text',
