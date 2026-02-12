2d56f389e1037d4baa5c4dd8bc944ee0ea95adee
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-02-12 14:21:29 +0100

    renamed plugin. name starts now with uppercase letter

--- pslzme.php
+++ pslzme.php
@@ -13,7 +13,7 @@
  * @package           pslzme
  *
  * @wordpress-plugin
- * Plugin Name:       pslzme
+ * Plugin Name:       Pslzme
  * Plugin URI:        https://www.pslzme.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
