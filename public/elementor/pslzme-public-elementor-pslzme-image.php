56723cbd41ec473149579a8df26c15f3e9940070
RobinDort <75115265+RobinDort@users.noreply.github.com>
2026-03-26 08:08:49 +0100

    Added new repeater controls to the pslzme image element instead of the old unpersonalized and personalized texts

--- public/elementor/pslzme-public-elementor-pslzme-image.php
+++ public/elementor/pslzme-public-elementor-pslzme-image.php
@@ -63,25 +63,267 @@ class ElementorWidgetPslzmeImage extends \Elementor\Widget_Base {
     private function add_content_controls(): void {
 
         $this->start_controls_section(
-            'content_section_pslzme_image',
+            'content_section_pslzme_image_unpersonalized',
             [
-                'label' => esc_html__('Pslzme Image Section', 'pslzme'),
+                'label' => esc_html__('Pslzme Image Unpersonalized Section', 'pslzme'),
                 'tab' 	=> \Elementor\Controls_Manager::TAB_CONTENT, 
             ]
         );
 
-        $this->add_control('pslzme_image_personalized_text',
+        $this->add_control(
+            'pslzme_image_unpersonalized_texts',
             [
-                'label' => esc_html__( 'Personalized Text', 'pslzme' ),
-                'type' => \Elementor\Controls_Manager::WYSIWYG,
+                'label' => esc_html__('Unpersonalized Texts', 'pslzme'),
+                'type' => \Elementor\Controls_Manager::REPEATER,
+                'fields' => [
+                    [
+                        'name' => 'text',
+                        'label' => esc_html__('Unpersonalized Text', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::WYSIWYG,
+                        'default' => esc_html__('Personalized text', 'pslzme'),
+                        'show_label' => false,
+                    ],
+                    [
+                        'name' => 'horizontal_position',
+                        'label' => esc_html__('Horizontal Position', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::CHOOSE,
+                        'options' => [
+                            'left' => [
+                                'title' => esc_html__('Left', 'pslzme'),
+                                'icon' => 'eicon-h-align-left',
+                            ],
+                            'center' => [
+                                'title' => esc_html__('Center', 'pslzme'),
+                                'icon' => 'eicon-h-align-center',
+                            ],
+                            'end' => [
+                                'title' => esc_html__('Right', 'pslzme'),
+                                'icon' => 'eicon-h-align-right',
+                            ],
+                        ],
+                        'default' => 'left',
+                        'toggle' => true,
+                    ],
+                    [
+                        'name' => 'vertical_position',
+                        'label' => esc_html__('Vertical Position', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::CHOOSE,
+                        'options' => [
+                            'start' => [
+                                'title' => esc_html__('Top', 'pslzme'),
+                                'icon' => 'eicon-v-align-top',
+                            ],
+                            'center' => [
+                                'title' => esc_html__('Middle', 'pslzme'),
+                                'icon' => 'eicon-v-align-middle',
+                            ],
+                            'end' => [
+                                'title' => esc_html__('Bottom', 'pslzme'),
+                                'icon' => 'eicon-v-align-bottom',
+                            ],
+                        ],
+                        'default' => 'start',
+                        'toggle' => true,
+                    ],
+                    [
+                        'name' => 'text_spacing',
+                        'label' => esc_html__('Pslzme image text spacing', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                        'size_units' => [ 'px', '%', 'em', 'rem' ],
+                        'default' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                    ],
+                    [
+                        'name' => 'text_padding',
+                        'label' => esc_html__('Pslzme image text padding', 'pslzme'),
+                        'type' =>  \Elementor\Controls_Manager::DIMENSIONS,
+                        'size_units' => [ 'px', '%', 'em', 'rem' ],
+                        'default' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                    ],
+                    [
+                        'name' => 'text_color',
+                        'label' => esc_html('Pslzme image text color', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::COLOR,
+                        'default' => '#000000'
+                    ]
+                ],
+                'default' => [
+                    [
+                        'text' => esc_html__('Unpersonalized text example #1', 'pslzme'),
+                        'horizontal_position' => 'left',
+                        'vertical_position' => 'start',
+                        'text_spacing' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                        'text_padding' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                        'text_color' => '#000000'
+                    ],
+                ],
+                'title_field' => '{{{ text }}}',
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'content_section_pslzme_image_personalized',
+            [
+                'label' => esc_html__('Pslzme Image Personalized Section', 'pslzme'),
+                'tab' 	=> \Elementor\Controls_Manager::TAB_CONTENT, 
             ]
         );
 
         $this->add_control(
-            'pslzme_image_unpersonalized_text',
+            'pslzme_image_personalized_texts',
             [
-                'label' => esc_html__( 'Unpersonalized Text', 'pslzme' ),
-                'type' => \Elementor\Controls_Manager::WYSIWYG,
+                'label' => esc_html__('Personalized Texts', 'pslzme'),
+                'type' => \Elementor\Controls_Manager::REPEATER,
+                'fields' => [
+                    [
+                        'name' => 'text',
+                        'label' => esc_html__('Personalized Text', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::WYSIWYG,
+                        'default' => esc_html__('Personalized text', 'pslzme'),
+                        'show_label' => false,
+                    ],
+                     [
+                        'name' => 'horizontal_position',
+                        'label' => esc_html__('Horizontal Position', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::CHOOSE,
+                        'options' => [
+                            'left' => [
+                                'title' => esc_html__('Left', 'pslzme'),
+                                'icon' => 'eicon-h-align-left',
+                            ],
+                            'center' => [
+                                'title' => esc_html__('Center', 'pslzme'),
+                                'icon' => 'eicon-h-align-center',
+                            ],
+                            'end' => [
+                                'title' => esc_html__('Right', 'pslzme'),
+                                'icon' => 'eicon-h-align-right',
+                            ],
+                        ],
+                        'default' => 'left',
+                        'toggle' => true,
+                    ],
+                    [
+                        'name' => 'vertical_position',
+                        'label' => esc_html__('Vertical Position', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::CHOOSE,
+                        'options' => [
+                            'start' => [
+                                'title' => esc_html__('Top', 'pslzme'),
+                                'icon' => 'eicon-v-align-top',
+                            ],
+                            'center' => [
+                                'title' => esc_html__('Middle', 'pslzme'),
+                                'icon' => 'eicon-v-align-middle',
+                            ],
+                            'end' => [
+                                'title' => esc_html__('Bottom', 'pslzme'),
+                                'icon' => 'eicon-v-align-bottom',
+                            ],
+                        ],
+                        'default' => 'start',
+                        'toggle' => true,
+                    ],
+                    [
+                        'name' => 'text_spacing',
+                        'label' => esc_html__('Pslzme image text spacing', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::DIMENSIONS,
+                        'size_units' => [ 'px', '%', 'em', 'rem' ],
+                        'default' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                    ],
+                    [
+                        'name' => 'text_padding',
+                        'label' => esc_html__('Pslzme image text padding', 'pslzme'),
+                        'type' =>  \Elementor\Controls_Manager::DIMENSIONS,
+                        'size_units' => [ 'px', '%', 'em', 'rem' ],
+                        'default' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                    ],
+                    [
+                        'name' => 'text_color',
+                        'label' => esc_html('Pslzme image text color', 'pslzme'),
+                        'type' => \Elementor\Controls_Manager::COLOR,
+                        'default' => '#000000'
+                    ]
+                ],
+                'default' => [
+                    [
+                        'text' => esc_html__('Unpersonalized text example #1', 'pslzme'),
+                        'horizontal_position' => 'left',
+                        'vertical_position' => 'start',
+                        'text_spacing' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                        'text_padding' => [
+                            'top' => 0,
+                            'right' => 0,
+                            'bottom' => 0,
+                            'left' => 0,
+                            'unit' => 'px',
+                            'isLinked' => false,
+                        ],
+                        'text_color' => '#000000'
+                    ],
+                ],
+                'title_field' => '{{{ text }}}',
+            ]
+        );
+
+        $this->end_controls_section();
+
+
+        $this->start_controls_section(
+            'pslzme_image_settings_section',
+            [
+                'label' => esc_html__('Pslzme Image Settings', 'pslzme'),
+                'tab' 	=> \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
 
@@ -102,31 +344,6 @@ class ElementorWidgetPslzmeImage extends \Elementor\Widget_Base {
                 'selectors' => [
 					'{{WRAPPER}} .pslzme-ov-image-container' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
-            ]
-        );
-
-        $this->add_responsive_control(
-            'pslzme_image_text_spacing',
-            [
-                'label' => esc_html__( 'Pslzme image text spacing', 'pslzme' ),
-                'type' => \Elementor\Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', '%', 'em', 'rem' ],
-                'default' => [
-                    'top' => 0,
-                    'right' => 0,
-                    'bottom' => 0,
-                    'left' => 0,
-                    'unit' => 'px',
-                    'isLinked' => false,
-                ],
-                'selectors' => [
-                    '{{WRAPPER}} .pslzme-ov-image-container .layered-text' => '
-                        margin-top: {{TOP}}{{UNIT}};
-                        margin-right: {{RIGHT}}{{UNIT}};
-                        margin-bottom: {{BOTTOM}}{{UNIT}};
-                        margin-left: {{LEFT}}{{UNIT}};
-                    ',
-                ],
             ]
         );
 
--- public/partials/pslzme-public-elementor-pslzme-image.php
+++ public/partials/pslzme-public-elementor-pslzme-image.php
@@ -21,6 +21,9 @@ $varsSet = $decryptionController->vars_set();
 $personalized_text = $settings['pslzme_image_personalized_text'] ?? '';
 $unpersonalized_text = $settings['pslzme_image_unpersonalized_text'] ?? '';
 
+$personalized_texts = $settings['pslzme_image_personalized_texts'] ?? [];
+$unpersonalized_texts = $settings['pslzme_image_unpersonalized_texts'] ?? [];
+
 $background_image_id    = $settings['pslzme_image_background']['id'] ?? '';
 $background_image_url   = $settings['pslzme_image_background']['url'] ?? '';
 $background_image_size  = $settings['pslzme_image_background_size'] ?? 'full';
@@ -48,18 +51,84 @@ $foreground_image_title = $settings['pslzme_image_foreground_title'] ?? '';
 
     <?php endif; ?>
 
-    <?php if($varsSet && $personalized_text) : ?>
+    <?php if($varsSet && !empty($personalized_texts)): ?>
 
-         <div class="pslzme_image ce_text block layered-text">
-            <?= wp_kses_post($personalized_text) ?>
-        </div>
+        <?php foreach ($personalized_texts as $ptItem):
+            $ptext = $ptItem['text'] ?? '';
+            $horizontal_alignment = $ptItem['horizontal_position'] ?? 'left';
+            $vertical_alignment = $ptItem['vertical_position'] ?? 'start';
+            $text_color = $uptItem['text_color'] ?? '#000000';
+            $text_spacing = $uptItem['text_spacing'] ?? [];
+            $unit = $text_spacing['unit'] ?? 'px';
+            $top = $text_spacing['top'] ?? 0;
+            $right = $text_spacing['right'] ?? 0;
+            $bottom = $text_spacing['bottom'] ?? 0;
+            $left = $text_spacing['left'] ?? 0;
+            $text_padding = $uptItem['text_padding'] ?? [];
+            $punit = $text_padding['unit'] ?? 'px';
+            $ptop = $text_padding['top'] ?? 0;
+            $pright = $text_padding['right'] ?? 0;
+            $pbottom = $text_padding['bottom'] ?? 0;
+            $pleft = $text_padding['left'] ?? 0;
+        ?>
+            <div class="pslzme_image ce_text block layered-text"
+                style="display: flex;
+                justify-content: <?= esc_attr($horizontal_alignment); ?>;
+                align-items: <?= esc_attr($vertical_alignment); ?>;
+                color: <?= esc_attr($text_color); ?>;
+                margin: <?= esc_attr($top) . esc_attr($unit) ?> 
+                        <?= esc_attr($right) . esc_attr($unit) ?> 
+                        <?= esc_attr($bottom) . esc_attr($unit) ?> 
+                        <?= esc_attr($left) . esc_attr($unit) ?>;
+                padding:<?= esc_attr($ptop) . esc_attr($punit) ?> 
+                        <?= esc_attr($pright) . esc_attr($punit) ?> 
+                        <?= esc_attr($pbottom) . esc_attr($punit) ?> 
+                        <?= esc_attr($pleft) . esc_attr($punit) ?>;">
+                <?= wp_kses_post($ptext) ?>
+            </div>
 
+        <?php endforeach; ?>
     <?php else: ?>
 
-        <div class="pslzme_image ce_text block layered-text">
-            <?= wp_kses_post($unpersonalized_text) ?>
-        </div>
+        <div class="pslzme-image-flex-container">
 
+            <?php foreach ($unpersonalized_texts as $uptItem):
+                $upText = $uptItem['text'];
+                $horizontal_alignment = $uptItem['horizontal_position'] ?? 'left';
+                $vertical_alignment = $uptItem['vertical_position'] ?? 'start';
+                $text_spacing = $uptItem['text_spacing'] ?? [];
+                $text_color = $uptItem['text_color'] ?? '#000000';
+                $unit = $text_spacing['unit'] ?? 'px';
+                $top = $text_spacing['top'] ?? 0;
+                $right = $text_spacing['right'] ?? 0;
+                $bottom = $text_spacing['bottom'] ?? 0;
+                $left = $text_spacing['left'] ?? 0;
+                $text_padding = $uptItem['text_padding'] ?? [];
+                $punit = $text_padding['unit'] ?? 'px';
+                $ptop = $text_padding['top'] ?? 0;
+                $pright = $text_padding['right'] ?? 0;
+                $pbottom = $text_padding['bottom'] ?? 0;
+                $pleft = $text_padding['left'] ?? 0;
+            ?>
+                <div class="pslzme_image ce_text block layered-text" 
+                    style="display: flex;
+                    justify-content: <?= esc_attr($horizontal_alignment); ?>;
+                    align-items: <?= esc_attr($vertical_alignment); ?>;
+                    color: <?= esc_attr($text_color); ?>;
+                    margin: <?= esc_attr($top) . esc_attr($unit) ?> 
+                            <?= esc_attr($right) . esc_attr($unit) ?> 
+                            <?= esc_attr($bottom) . esc_attr($unit) ?> 
+                            <?= esc_attr($left) . esc_attr($unit) ?>;
+                    padding:<?= esc_attr($ptop) . esc_attr($punit) ?> 
+                        <?= esc_attr($pright) . esc_attr($punit) ?> 
+                        <?= esc_attr($pbottom) . esc_attr($punit) ?> 
+                        <?= esc_attr($pleft) . esc_attr($punit) ?>;">
+                    <?= wp_kses_post($upText) ?>
+                </div>
+
+            <?php endforeach; ?>
+
+        </div>
     <?php endif; ?>
 
     <?php if($foreground_image_url) : ?>
